3643d5524e2e0306781a198e91e7afd8f9a9a8e3
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-08-07 04:37:19 +0800

    Add files via upload

---
+++ Act10.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] height = new int[6];
+            int ShortCount = 0;
+            int AverageCount = 0;
+            int TallCount = 0;
+
+           Console.WriteLine("Enter the heights of 6 people in cm:");
+            for (int i = 0; i < height.Length; i++)
+            {
+                Console.Write($"Height of person {i + 1}: ");
+                height[i] = int.Parse(Console.ReadLine());
+                if (height[i] < 160)
+                {
+                    ShortCount++;
+                }
+                else if (height[i] >= 160 && height[i] <= 180)
+                {
+                    AverageCount++;
+                }
+                else
+                {
+                    TallCount++;
+                }
+            }
+            Console.WriteLine($"\nNumber of short people (<160 cm): {ShortCount}");
+            Console.WriteLine($"Number of average height people (160-180 cm): {AverageCount}");
+            Console.WriteLine($"Number of tall people (>180 cm): {TallCount}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Act11.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = new string[10];
+            int[] ages = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter name {i + 1}: ");
+                names[i] = Console.ReadLine();
+
+                Console.Write($"Enter age {i + 1}: ");
+                string input = Console.ReadLine();
+                int age = int.Parse(input);
+                ages[i] = age;
+            }
+
+            Console.WriteLine("\nVoter Eligibility and Categorization:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Name: {names[i]}, Age: {ages[i]} - ");
+
+                if (ages[i] < 18)
+                {
+                    Console.WriteLine("Not eligible to vote.");
+                }
+                else if (ages[i] <= 30)
+                {
+                    Console.WriteLine("Young Voter");
+                }
+                else if (ages[i] <= 59)
+                {
+                    Console.WriteLine("Mid-age Voter");
+                }
+                else
+                {
+                    Console.WriteLine("Senior Voter");
+                }
+            }
+        }
+    }
+}
