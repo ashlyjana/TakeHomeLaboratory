5cf64371d721243fb8e28bbbd298cd5d64a3f603
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-08-09 06:12:35 +0800

    Add files via upload

---
+++ Act16.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] symptoms = new string[5];
+
+            for (int i = 0; i < symptoms.Length; i++)
+            {
+                Console.WriteLine("Enter symptom " + (i + 1) + ":");
+                symptoms[i] = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("You have entered the following symptoms:");
+
+            for (int i = 0; i < symptoms.Length; i++)
+            {
+                Console.WriteLine(symptoms[i]);
+
+                string department;
+
+                switch (symptoms[i])
+                {
+                    case "fever":
+                        department = "General Medicine";
+                        break;
+                    case "cough":
+                        department = "General Medicine";
+                        break;
+                    case "injury":
+                        department = "Emergency";
+                        break;
+                    case "burn":
+                        department = "Emergency";
+                        break;
+                    default:
+                        department = "Refer to Specialist";
+                        break;
+                }
+
+                Console.WriteLine($"Patient {i + 1} ({symptoms[i]}) belongs to  {department}");
+            }
+        }
+    }
+}
---
+++ Act17.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[8];
+            Console.WriteLine(" Enter a 8 whole numbers:");
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.Write($"Number {i + 1}:");
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Number Categorys:");
+
+            for (int i = 0; i < 8; i++)
+            {
+
+                int num = numbers[i];
+                string binary = Convert.ToString(num);
+                string evenOdd = "Odd";
+                if (num % 2 == 0)
+                    evenOdd = "Even";
+                string PrimeStatus = "no";
+      
+                if (IsPrime(num))
+                    PrimeStatus = "yes";
+
+                Console.WriteLine("Number:" + num);
+                Console.WriteLine("Binary:" + binary);
+                Console.WriteLine("Even or Odd:" + evenOdd);
+                Console.WriteLine("Prime:" + PrimeStatus);
+            }
+             bool IsPrime(int number)
+            {
+                if (number <= 1) return false;
+                for (int i = 2; i <= number / 2; i++) { 
+                    if (number % i == 0) return false;
+                }
+                return true;
+            }
+
+
+        }
+    }
+}
