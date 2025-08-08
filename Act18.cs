d11dc9356fc38777340cc00be42b9b0544de53bf
Quising, Ashly Jana A. <ashlyjanaquising@gmail.com>
2025-08-09 06:24:18 +0800

    Add files via upload

---
+++ Act18.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] passwords = new string[5];
+
+            for (int i = 0; i < passwords.Length; i++)
+            {
+                Console.Write($"Enter password {i + 1}: ");
+                passwords[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Password Strength Results:");
+
+            for (int i = 0; i < passwords.Length; i++)
+            {
+                string pwd = passwords[i];
+                bool hasUpper = false, hasDigit = false, hasSpecial = false;
+
+                for (int j = 0; j < pwd.Length; j++)
+                {
+                    char c = pwd[j];
+                    if (char.IsUpper(c))
+                        hasUpper = true;
+                    else if (char.IsDigit(c))
+                        hasDigit = true;
+                    else if ("!@#$%^&*()_+=-[]{};:'\",.<>?/\\|".Contains(c))
+                        hasSpecial = true;
+                }
+
+                if (pwd.Length >= 8)
+                {
+                    if (hasUpper)
+                    {
+                        if (hasDigit)
+                        {
+                            if (hasSpecial)
+                            {
+                                Console.WriteLine($"Password {i + 1}: Strong");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Password {i + 1}: Weak (No special character)");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Password {i + 1}: Weak (No digit)");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Password {i + 1}: Weak (No uppercase letter)");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Password {i + 1}: Invalid (Too short)");
+                }
+            }
+        }
+    }
+}
---
+++ Act19.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] categories = { "Food", "Rent", "Utilities", "Transport", "Others" };
+            double[] expenses = new double[5];
+            double monthlyIncome = 25000;
+            double total = 0;
+            int maxIndex = 0;
+
+            for (int i = 0; i < categories.Length; i++)
+            {
+                Console.Write($"Enter budget for {categories[i]}: ");
+                expenses[i] = double.Parse(Console.ReadLine());
+                total += expenses[i];
+
+                if (expenses[i] > expenses[maxIndex])
+                {
+                    maxIndex = i;
+                }
+            }
+
+            Console.WriteLine($"\nTotal Budget: {total}");
+
+            if (total > monthlyIncome)
+            {
+                Console.WriteLine("Over budget!");
+                Console.WriteLine($"Suggestion: Reduce expenses in '{categories[maxIndex]}' (Highest expense)");
+            }
+            else
+            {
+                Console.WriteLine("Within budget");
+            }
+
+        }
+    }
+}
